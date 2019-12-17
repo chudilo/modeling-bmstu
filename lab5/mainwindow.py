5fa5ad867cdf7bb37f0e6aa88cc18d571e3365f3
chudilo <chudik97@yandex.ru>
2019-12-17 11:44:54 +0300

    Final version of lab5

--- lab5/mainwindow.py
+++ lab5/mainwindow.py
@@ -100,11 +100,11 @@ class ServingMachineStatistics(QWidget):
         self.numLabel = QLabel("Number of requests")
         self.numLine = QLineEdit("300")
         self.failLabel = QLabel("Failed requests")
-        self.failLine = QLabel()
+        self.failLine = QLabel("61")
         self.failLine.setFrameShape(QFrame.WinPanel)
         self.failLine.setFrameShadow(QFrame.Raised)
         self.probLabel = QLabel("Probability of failing")
-        self.probLine = QLabel()
+        self.probLine = QLabel("0.2033")
         self.probLine.setFrameShape(QFrame.WinPanel)
         self.probLine.setFrameShadow(QFrame.Raised)
         self.answerBox = QFormLayout()
@@ -114,9 +114,9 @@ class ServingMachineStatistics(QWidget):
         self.answerBox.addRow(self.probLabel, self.probLine)
 
         self.button = QPushButton("Calculate")
-        #self.button.clicked.connect(self.calculate)
-        self.button.setMinimumSize(100,65)
-        self.button.setMaximumSize(200, 65)
+        self.button.clicked.connect(self.calculate)
+        self.button.setMinimumSize(150,65)
+        self.button.setMaximumSize(250, 65)
         self.buttonLayout = QVBoxLayout()
         self.buttonLayout.addWidget(self.button)
         self.buttonLayout.setAlignment(Qt.AlignVCenter)
@@ -136,164 +136,96 @@ class ServingMachineStatistics(QWidget):
 
         self.setLayout(self.mainLayout)
 
-
-        """
-        self.processorLabel = QLabel("Processor")
-        self.aLabel = QLabel("a")
-        self.aLine = QLineEdit("5")
-        self.bLabel = QLabel("b")
-        self.bLine = QLineEdit("15")
-        self.procBox = QFormLayout()
-        self.procBox.addWidget(self.processorLabel)
-        self.procBox.addRow(self.aLabel, self.aLine)
-        self.procBox.addRow(self.bLabel, self.bLine)
-
-        self.genBox.setSpacing(10)
-        self.procBox.setSpacing(10)
-        self.dataLayout.addLayout(self.genBox)
-        self.dataLayout.addLayout(self.procBox)
-        self.dataLayout.setSpacing(30)
-
-        self.hLayout = QHBoxLayout()
-
-        self.numLayout = QFormLayout()
-        self.numLabel = QLabel("Number of requests")
-        self.numLine = QLineEdit("10000")
-        self.numLayout.addWidget(self.numLabel)
-        self.numLayout.addWidget(self.numLine)
-
-        self.deltLayout = QFormLayout()
-        self.deltLabel = QLabel("\u0394t")
-        self.deltLine = QLineEdit("1")
-        self.deltLayout.addWidget(self.deltLabel)
-        self.deltLayout.addWidget(self.deltLine)
-
-        self.probLayout = QFormLayout()
-        self.probLabel = QLabel("Probability of returning")
-        self.probLine = QLineEdit("0")
-        self.probLayout.addWidget(self.probLabel)
-        self.probLayout.addWidget(self.probLine)
-
-
-        self.numLayout.setSpacing(10)
-        self.deltLayout.setSpacing(10)
-        self.probLayout.setSpacing(10)
-
-        self.hLayout.addLayout(self.numLayout)
-        self.hLayout.addLayout(self.deltLayout)
-        self.hLayout.addLayout(self.probLayout)
-        self.hLayout.setSpacing(30)
-
-        line = QFrame()
-        line.setFrameShape(QFrame.HLine)
-        line.setFrameShadow(QFrame.Sunken)
-
-        line1 = QFrame()
-        line1.setFrameShape(QFrame.HLine)
-        line1.setFrameShadow(QFrame.Sunken)
-
-        self.answerLabel = QLabel("Optimal length of queue")
-        self.dtLabel = QLabel("\u0394t")
-        self.dtLine = QLabel()
-        self.dtLine.setFrameShape(QFrame.WinPanel)
-        self.dtLine.setFrameShadow(QFrame.Raised)
-        self.eventLabel = QLabel("Events")
-        self.eventLine = QLabel()
-        self.eventLine.setFrameShape(QFrame.WinPanel)
-        self.eventLine.setFrameShadow(QFrame.Raised)
-        self.answerBox = QFormLayout()
-        self.answerBox.addWidget(self.answerLabel)
-        self.answerBox.addRow(self.dtLabel, self.dtLine)
-        self.answerBox.addRow(self.eventLabel, self.eventLine)
-
-        self.button = QPushButton("Calculate")
-        self.button.clicked.connect(self.calculate)
-        self.button.setMinimumSize(100,65)
-        self.button.setMaximumSize(200, 65)
-        self.buttonLayout = QVBoxLayout()
-        self.buttonLayout.addWidget(self.button)
-        self.buttonLayout.setAlignment(Qt.AlignBottom)
-
-        self.answLayout = QHBoxLayout()
-        self.answLayout.addLayout(self.answerBox)
-        self.answLayout.addLayout(self.buttonLayout)
-        #self.answLayout.setAlignment(Qt.AlignBottom)
-
-        self.mainLayout.addLayout(self.dataLayout)
-        self.mainLayout.addWidget(line)
-        self.mainLayout.addLayout(self.hLayout)
-        self.mainLayout.addWidget(line1)
-        self.mainLayout.addLayout(self.answLayout)
-
-        self.mainLayout.setAlignment(Qt.AlignVCenter)
-        self.mainLayout.setSpacing(20)
-        """
-
-
-
     def calculate(self):
-        m = self.mLine.text()
-        try:
-            m = float(m)
-            if m < 0:
-                return
-        except Exception as e:
-            print(e)
+        mGen = float(self.mLine.text())
+        sigmGen = float(self.sigmaLine.text())
 
-        sigma = self.sigmaLine.text()
-        try:
-            sigma = float(sigma)
-            if math.isclose(sigma, 0.0, rel_tol=1e-07, abs_tol=0.0):
-                return
-        except Exception as e:
-            print(e)
+        mOper1 = float(self.mLine1.text())
+        sigmOper1 = float(self.sigmaLine1.text())
+        mOper2 = float(self.mLine2.text())
+        sigmOper2 = float(self.sigmaLine2.text())
+        mOper3 = float(self.mLine3.text())
+        sigmOper3 = float(self.sigmaLine3.text())
 
-        requestsCount = self.numLine.text()
-        try:
-            requestsCount = int(requestsCount)
-            if requestsCount < 0:
-                return
+        tComp1 = float(self.tLine1.text())
+        tComp2 = float(self.tLine2.text())
 
-        except Exception as e:
-            print(e)
+        n = int(self.numLine.text())
 
-        a = self.aLine.text()
-        try:
-            a = float(a)
-            if a < 0:
-                return
-        except Exception as e:
-            print(e)
+        #print(mGen, sigmGen)
+        #print(mOper1, sigmOper1)
+        #print(mOper2, sigmOper2)
+        #print(mOper3, sigmOper3)
+        #print(tComp1, tComp2, n)
 
-        b = self.bLine.text()
-        try:
-            b = float(b)
-            if b < 0:
-                return
-        except Exception as e:
-            print(e)
+        requestsFailed = runModel(n, Generator(mGen, sigmGen), Generator(mOper1, sigmOper1),
+                 Generator(mOper2, sigmOper2), Generator(mOper3, sigmOper3),
+                 Computer(tComp1), Computer(tComp2))
 
-        dt = self.deltLine.text()
-        try:
-            dt = float(dt)
-            if dt < 0 or math.isclose(dt, 0.0, rel_tol=1e-07, abs_tol=0.0):
-                return
-        except Exception as e:
-            print(e)
+        self.failLine.setText(str(requestsFailed))
+        self.probLine.setText("{:.4}".format(requestsFailed/(n)))
 
-        pRet = self.probLine.text()
-        try:
-            pRet = float(pRet)
-            if not -0.00000000001 < pRet <= 1.000000001:
-                return
-        except Exception as e:
-            print(e)
 
-        generator = Generator(m, sigma)
-        processor = Processor(a, b)
+def runModel(n, gen, op0, op1, op2, comp0, comp1):
+    generator = gen.getWorkTime()
+    operators = [None, None, None]
+    computers = [[None], [None]]
 
-        self.dtLine.setText(str(getDeltaQueue(generator, processor, requestsCount, dt, pRet)))
-        self.eventLine.setText(str(getEventsQueue(generator, processor, requestsCount, pRet)))
+    requestsFailed = 0
+    requestsCount = 1
+    peopleCount = 1
+
+    while peopleCount < n:
+        m = generator
+        event = 0
+        for i in range(len(operators)):
+            if operators[i] is not None:
+                if operators[i] < m:
+                    m = operators[i]
+                    index = i
+                    event = 1
+
+        for i in range(len(computers)):
+            if computers[i][0] is not None:
+                if min(computers[i]) < m:
+                    m = computers[i][0]
+                    index = i
+                    event = 2
+
+        if event == 0:
+            if operators[0] is None:
+                operators[0] = generator + op0.getWorkTime()
+            elif operators[1] is None:
+                operators[1] = generator + op1.getWorkTime()
+            elif operators[2] is None:
+                operators[2] = generator + op2.getWorkTime()
+            else:
+                requestsFailed += 1
+
+            generator += gen.getWorkTime()
+            peopleCount += 1
+
+        elif event == 1:
+            if index == 2:
+                if computers[1][-1] is None:
+                    computers[1] = []
+                    computers[1].append(operators[index] + comp1.getWorkTime())
+
+            else:
+                if computers[0][-1] is None:
+                    computers[0] = []
+                    computers[0].append(operators[index] + comp0.getWorkTime())
+
+            operators[index] = None
+
+        elif event == 2:
+            computers[index].pop(0)
+            if not computers[index]:
+                computers[index] = [None]
+
+            requestsCount += 1
+
+    return requestsFailed
 
 
 class Generator(object):
@@ -302,21 +234,16 @@ class Generator(object):
         self.__sigma = sigma
 
     def getWorkTime(self):
-        ret = -1
-        while ret < 0:
-            ret = (sum([random.random() for _ in range(12)]) - 6)*self.__sigma + self.__m
-            #ret = numpy.random.normal(self.__m, self.__sigma)
-        return ret
+        res = self.__m + (random.random() - 0.5) * 2 * self.__sigma
+        return res
 
 
-class Processor(object):
-    def __init__(self, a, b):
-        self.__a = a
-        self.__b = b
+class Computer(object):
+    def __init__(self, t):
+        self.__t = t
 
     def getWorkTime(self):
-        ret = self.__a + random.random()*(self.__b - self.__a)
-        return ret
+        return self.__t
 
 
 class RequestQueue(object):
@@ -336,267 +263,11 @@ class RequestQueue(object):
         if self.max < self.l:
             self.max = self.l
 
-def getDeltaQueue(generator, processor, requestsCount, dt, pRet):
-    print("START DELTA")
-    currTime = dt
-    doneCount = 1
-
-    queue = RequestQueue()
-    generatorTime = generator.getWorkTime()
-    queue.inc()
-
-    processorTime = generatorTime + processor.getWorkTime()
-    queue.dec()
-    inactionF = False
-
-    while doneCount < requestsCount:
-        if currTime > generatorTime:
-            generatorTime += generator.getWorkTime()
-            queue.inc()
-
-        if currTime > processorTime:
-            if inactionF:
-                if queue.dec():
-                    processorTime = generatorTime + processor.getWorkTime()
-                    #print("dec")
-                    doneCount += 1
-                    if random.random() < pRet:
-                        queue.inc()
-                        processorTime += processor.getWorkTime()
-                        queue.dec()
-
-                    inactionF = False
-            else:
-                if queue.dec():
-                    #print("dec")
-                    processorTime += processor.getWorkTime()
-                    doneCount += 1
-                    if random.random() < pRet:
-                        queue.inc()
-                        processorTime += + processor.getWorkTime()
-                        queue.dec()
-                else:
-                    inactionF = True
-
-
-        currTime += dt
-
-    return queue.max
-
-def getEventsQueue(generator, processor, requestsCount, pRet):
-    print("START EVENT")
-    doneCount = 1
-
-    queue = RequestQueue()
-    generatorTime = [generator.getWorkTime()]
-    queue.inc()
-
-    processorTime = [generatorTime[0] + processor.getWorkTime()]
-    queue.dec()
-    inactionF = False
-
-    returnedRequests = []
-
-    # Я ЗАПРОГАЛ ЭТО В 4 ЧАСА НОЧИ, ДА ЛАДНО ВАМ
-    events = [generatorTime, processorTime]
-
-    while doneCount < requestsCount:
-        cur = []
-        for i in range(len(events)):
-            if events[i]:
-                cur.append((events[i][0], i))
-
-        mVal = cur[0][0]
-        mIndex = cur[0][1]
-
-        for i in range(len(cur)):
-            if cur[i][0] < mVal:
-                mVal, mIndex = cur[i][0], cur[i][1]
-
-        event = events[mIndex][0]
-        doneCount += 1
-
-        if mIndex == 0:
-            events[0].append(events[0][0] + generator.getWorkTime())
-            queue.inc()
-            events[0].pop(0)
-
-        elif mIndex == 1:
-            if inactionF:
-                if queue.dec():
-                    events[1].append(events[0][0] + processor.getWorkTime())
-                    doneCount += 1
-                    inactionF = False
-                    events[1].pop(0)
-
-                    if random.random() < pRet:
-                        queue.inc()
-                        events[1].append(events[1][0] + processor.getWorkTime())
-                        events[1].pop(0)
-                        #Здесь был костыль
-            else:
-                if queue.dec():
-                    events[1].append(events[1][0] + processor.getWorkTime())
-                    doneCount += 1
-                    events[1].pop(0)
-
-                    if random.random() < pRet:
-                        queue.inc()
-                        events[1].append(events[1][0] + processor.getWorkTime())
-                        events[1].pop(0)
-                else:
-                    inactionF = True
-                    events[1].append(events[0][-1])
-                    events[1].pop(0)
-
-
-    return queue.max
-
-'''
-    
-    def event_based_modelling(self, a, b, lmbda):
-        req_generator = PoissonGenerator(lmbda)
-        req_proccessor = UniformGenerator(a, b)
-    
-        req_done_count = 0
-        t_generation = req_generator.generate()
-        t_proccessor = t_generation + req_proccessor.generate()
-    
-        while t_proccessor < self.req_count:
-            if t_generation <= t_proccessor:
-                self.add_to_queue()
-                t_generation += req_generator.generate()
-            if t_generation >= t_proccessor:
-                req_done_count += self.rem_from_queue()
-                t_proccessor += req_proccessor.generate()
-    
-        return self.queue_len_max, req_done_count, self.reenter
-    
-    def time_based_modelling(self, a, b, lmbda):
-    
-        req_generator = PoissonGenerator(lmbda)
-        req_proccessor = UniformGenerator(a, b)
-    
-        req_done_count = 0
-        t_generation = req_generator.generate()
-        t_proccessor = t_generation + req_proccessor.generate()
-    
-        t_curr = 0
-        while t_curr < self.req_count:
-            # while t_curr < 5500:
-            if t_generation <= t_curr:
-                self.add_to_queue()
-                t_generation += req_generator.generate()
-            if t_proccessor <= t_curr:
-                req_done_count += self.rem_from_queue()
-                t_proccessor += req_proccessor.generate()
-    
-            t_curr += self.dt
-        return self.queue_len_max, req_done_count, self.reenter
-
-        self.layout = QHBoxLayout()
-        size = 5
-        self.table = QTableWidget(size, size)
-        self.table.setMaximumSize(275, 295)
-        self.table.setHorizontalHeaderLabels([str(i + 1) + "\n " for i in range(size)])
-
-        self.tableResults = QTableWidget(size, 2)
-        self.tableResults.horizontalHeader().setDefaultSectionSize(100)
-        self.tableResults.verticalHeader().setDefaultSectionSize(50)
-        self.tableResults.setMaximumSize(225, 295)
-        self.tableResults.setHorizontalHeaderLabels(["Предельные\nвероятности", "Время\nстабилизации"])
-
-        #self.table.setCellWidget(1000, 1000, self)
-        self.table.horizontalHeader().setDefaultSectionSize(50)
-        self.table.verticalHeader().setDefaultSectionSize(50)
-        #self.table.setCellWidget(200, 200, self.table)
-        delegate = IconDelegate(self.table)
-        self.table.setItemDelegate(delegate)
-        self.tableLayout = QHBoxLayout()
-        self.tableLayout.addWidget(self.tableResults)
-        tmp = QHBoxLayout()
-        tmp.addWidget(self.table)
-        tmp.setMargin(10)
-
-        self.tableLayout.addLayout(tmp)
-        self.tableLayout.setAlignment(Qt.AlignRight)
-
-        self.panel = QVBoxLayout()
-
-        self.cell = QLineEdit()
-        self.cell.setMaximumSize(150, 20)
-
-        self.buttonSize = QPushButton("Задать размер")
-        self.buttonSize.setMaximumSize(150, 50)
-        #self.buttonSize.clicked.connect(self.resizeTable)
-
-        self.buttonCalculate = QPushButton("Вычислить")
-        #self.buttonCalculate.clicked.connect(self.solve)
-        self.buttonCalculate.setMaximumSize(150, 50)
-
-        self.panel.addWidget(self.cell)
-        self.panel.addWidget(self.buttonSize)
-        self.panel.addWidget(self.buttonCalculate)
-        self.panel.setAlignment(Qt.AlignTop)
-
-        self.layout.addLayout(self.panel)
-        self.layout.addLayout(self.tableLayout)
-        self.layout.setAlignment(Qt.AlignTop)
-        #self.layout.setAlignment(Qt.AlignRight)
-
-        self.setLayout(self.layout)
-
-
-    def resizeTable(self):
-        print("fill")
-        try:
-            size = int(self.cell.text())
-        except ValueError:
-            print("Wrong value")
-            return
-
-        print("resize")
-        self.table.setColumnCount(size)
-        self.table.setRowCount(size)
-
-        self.tableResults.setRowCount(size)
-        self.fillTable(size)
-
-    def fillTable(self, size):
-        print("fill")
-        self.table.setMaximumSize(size*50 + 25, size*50 + 45)
-        self.tableResults.setMaximumSize(225, size * 50 + 45)
-        newFont = QFont("Arial", 12)
-
-        for i in range(size):
-            for j in range(size):
-                if i == j:
-                    #self.table.setItem(i, j, QTableWidgetItem(QIcon("sticker.svg"), ""))
-                    item = QTableWidgetItem("0")
-                    item.setTextAlignment(Qt.AlignCenter)
-                    item.setFont(newFont)
-                    self.table.setItem(i, j, item)
-                else:
-                    item = QTableWidgetItem(str(random.randrange(1)))
-                    item.setTextAlignment(Qt.AlignCenter)
-                    item.setFont(newFont)
-                    self.table.setItem(i, j, item)
-
-        self.table.setHorizontalHeaderLabels([str(i + 1) + "\n " for i in range(size)])
-        print(self.table.item(0, 1).text())
-
-        for i in range(size):
-            for j in range(2):
-                    item = QTableWidgetItem("__")
-                    item.setTextAlignment(Qt.AlignCenter)
-                    item.setFont(newFont)
-                    self.tableResults.setItem(i, j, item)
-    '''
 
 class MainWindow(QMainWindow):
     def __init__(self, widget):
         QMainWindow.__init__(self)
-        self.setWindowTitle("Обслуживающий аппарат")
+        self.setWindowTitle("Система массового обслуживанияи")
         self.resize(800, 600)
 
         # Menu
